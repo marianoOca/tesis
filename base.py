62ef4123568f1789038324ed8ba0e3610919eccf
El Marian <marianoagoca@gmail.com>
2024-09-22 17:21:00 -0300

    back up

---
+++ base.py
@@ -0,0 +1,159 @@
+# Requeriments: pip install Bio
+
+from Bio import SeqIO
+import random
+#seed = 10
+seed = 626019
+
+
+### AUX ###
+def save_list_to_file(l:list, out_file):
+    file = open(out_file, 'w')
+    for n in l:
+        file.write(f"{n}\n")
+    file.close()
+
+def read_list_from_file(in_file):
+    res = []
+    file =  open(in_file, 'r')
+    for line in file:
+        num = float(line.strip())
+        if num.is_integer():
+            num = int(num)
+        res.append(num)
+    file.close()
+    return res
+
+def write_with_size(seq:str, out_file, size:int = 100):
+    i = size
+    while i < len(seq):
+        out_file.write(seq[i-size:i] + "\n")
+        i = i + size
+    out_file.write(seq[i-size:] + "\n\n")
+
+def size_to_list_aux(seq_record:SeqIO.SeqRecord, res:list):
+    res.append(len(seq_record))
+
+def size_to_list(dataset):
+    size_list = []
+    map_bio(dataset, size_to_list_aux, size_list)
+    return size_list
+
+def filter_to_file(dataset, out_file):
+    f = open(out_file, "w")
+    for seq_record in SeqIO.parse(dataset, "fasta"):
+        if (len(seq_record) >= 50):
+            f.write(">" + seq_record.description + "\n")
+            write_with_size(str(seq_record.seq), f)
+    f.close()
+
+### MAP DATASET ###
+
+def map_bio(dataset, function, auxVar = False):
+    if auxVar == False:
+        for seq_record in SeqIO.parse(dataset, "fasta"):
+            function(seq_record)
+    else:
+        for seq_record in SeqIO.parse(dataset, "fasta"):
+            function(seq_record, auxVar)
+
+
+### sHOW ENTRY ###
+
+def show_entry(seq_record:SeqIO.SeqRecord):
+    print(seq_record.description)
+    print(repr(seq_record.seq))
+    print("Size: ", len(seq_record))
+
+
+### RESIZE ###
+
+def copy_with_size(dataset, out_file, size):
+    f = open(out_file, "a")
+    for seq_record in SeqIO.parse(dataset, "fasta"):
+        f.write(">" + seq_record.description + "\n")
+        write_with_size(str(seq_record.seq), f, size)
+    f.close()
+
+
+### SORT ###
+
+def save_to_list(seq_record:SeqIO.SeqRecord, seq_list:list):
+    seq_list.append(seq_record)
+
+def sort_to_file(dataset, out_file):
+    seq_list = []
+    map_bio(dataset, save_to_list, seq_list)
+    seq_list.sort(key = len) #ordena según su largo
+    
+    f = open(out_file, "a")
+    for seq_record in seq_list:
+        f.write(">" + seq_record.description + "\n")
+        write_with_size(str(seq_record.seq), f)
+    f.close()
+
+
+### SUFFLE ###
+
+def suffle_seq(seq_record:SeqIO.SeqRecord, out_file):
+    out_file.write(">" + seq_record.description + "\n")
+    seq_list = list(str(seq_record.seq))
+    random.shuffle(seq_list)
+    seq_ran = ''.join(seq_list) #no se xq no funciona con str(seq_list)
+    write_with_size(seq_ran, out_file)
+
+def suffle_to_file(dataset, out_file):
+    random.seed(seed)
+    f = open(out_file, "a")
+    map_bio(dataset, suffle_seq, f)
+    f.close()
+
+
+### RANDOM ###
+
+# Fuente: https://es.wikipedia.org/wiki/Formato_FASTA#Representación_de_la_secuencia
+def random_seq(seq_record:SeqIO.SeqRecord, out_file):
+    out_file.write(">" + seq_record.description + "\n")
+    size = len(seq_record)
+    #sample = list("TGAC") #este es para ls_orchid.fasta
+    sample = list("ACDEFGHIKLMNPQRSTVWY") #basado en proteinnet7.fasta, 20 aminoácidos
+    random_list = [random.choice(sample) for _ in range(size)]
+    random_seq = ''.join(random_list)
+    write_with_size(random_seq, out_file)
+
+def random_to_file(dataset, out_file):
+    random.seed(seed)
+    f = open(out_file, "w")
+    map_bio(dataset, random_seq, f)
+    f.close()
+
+
+### ICALC ###
+
+def icalc(seq:str):
+    b = []
+    for i in range(len(seq)):
+        mr = 0
+        for j in range(i, mr, -1):
+            if seq[j-mr:j] != seq[i-mr+1:i+1]:
+                continue
+            while i - mr >= 0 and seq[i-mr] == seq[j-mr-1]:
+                mr += 1
+        b.append(mr)
+    
+    res = 0
+    for i in range(len(b)):
+        res += 1.0 / (1.0 + b[i])
+    return res / len(b)
+
+def show_icalc(seq_record:SeqIO.SeqRecord):
+    print(seq_record.description)
+    print("Icalc: ", icalc(str(seq_record.seq)))
+
+def icalc_to_list_aux(seq_record:SeqIO.SeqRecord, res:list):
+    res.append(icalc(str(seq_record.seq)))
+
+def icalc_to_list(dataset):
+    icalc_list = []
+    map_bio(dataset, icalc_to_list_aux, icalc_list)
+    return icalc_list
